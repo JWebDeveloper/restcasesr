2044c9878e7e27966c4976030ac63923a3437212
Jatinder Kumar Full Stack Developer <101791804+JWebDeveloper@users.noreply.github.com>
2025-01-18 16:27:12 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,22 @@
-import LoginPage from '../Features/LoginPage/LoginPage';
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/login", // Redirects to the `/login` route
+      permanent: false,      // Marks the redirection as temporary
+    },
+  };
+}
 
 export default function Home() {
-  return <>
-  <LoginPage />;
-</>
+  // This component won't render due to redirection
+  return null
 }
+
+// // old working code
+// import LoginPage from '../Features/LoginPage/LoginPage';
+
+// export default function Home() {
+//   return <>
+//   <LoginPage />;
+// </>
+// }
